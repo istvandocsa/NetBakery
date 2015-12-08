34f8d849bc9d6cfa13dabc8f10a321a472202628
Balázs Wermer <wermerb@gmail.com>
2015-12-08 17:00:33 +0100

    added new functions + cleared up my act

--- app/scripts/product/cart/cart.service.js
+++ app/scripts/product/cart/cart.service.js
@@ -14,31 +14,66 @@
   function cartService($log) {
     var cart = [];
     var service = {
-      addToCart: addToCart,
-      removeFromCart: removeFromCart,
-      clearCart: clearCart,
+      add: add,
+      addOne: addOne,
+      removeOne: removeOne,
+      remove: remove,
+      clear: clear,
       getCart: getCart
     };
     return service;
 
     ////////////////
 
-    function addToCart(obj) {
-      cart.push(obj);
-      $log.debug(cart);
+    function add(obj) {
+      var contains = false;
+      $.each(cart, function () {
+        if ($(this)[0].name == obj.name) {
+          $log.info('Cart already contains the product. Adding amount.');
+          $(this)[0].amount += obj.amount;
+          contains = true;
+        }
+      });
+
+      if (!contains) {
+        cart.push(obj);
+        $log.info('Added product: ', obj);
+      }
+      console.log(cart);
     }
 
-    function removeFromCart(obj) {
-      cart = $.grep(cart, function (value) {
-        return value != obj;
+    function addOne(obj) {
+      $.each(cart, function () {
+        if (obj.name == $(this)[0].name) {
+          $(this)[0].amount++;
+          $log.info('Increased product amount by one. Product:', $(this)[0]);
+        }
+      });
+
+    }
+
+    function removeOne(obj) {
+      $.each(cart, function () {
+        if (obj.name == $(this)[0].name) {
+          $(this)[0].amount--;
+          $log.info('Decreased product amount by one. Product:', $(this)[0]);
+        }
       });
     }
 
-    function clearCart(){
+    function remove(obj) {
+      var index = cart.indexOf(obj);
+      cart.splice(index, 1);
+      $log.info('Removed product from cart. Product:', obj);
+    }
+
+    function clear() {
       cart = [];
+      $log.info('Emptied cart.');
     }
 
     function getCart() {
+      $log.info('Getting cart content.');
       return cart;
     }
   }
